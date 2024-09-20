131763cc1923045656d255edcb93aa08de807226
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-09-20 10:52:24 +0200

    Initial commit

---
+++ src/test/java/giorgiaipsarop/GestioneEventi/GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package giorgiaipsarop.GestioneEventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
